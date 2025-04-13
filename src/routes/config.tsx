5bc90c34550584e100eeb8ef5107da1a76223444
VietDucDev <vietducdo00@gmail.com>
2025-04-13 16:12:55 +0700

    [FE] Update config router

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -1,70 +1,62 @@
-import React, { JSX, lazy } from 'react';
-import DefaultLayout from '../layouts/DefaultLayout';
-import AdminLayout from '../layouts/AdminLayout';
-import NotFound from '../pages/NotFound';
-import { Navigate } from 'react-router-dom';
-import { isAuthenticated } from '../utils/auth';
+import React, { JSX, lazy } from "react";
+import DefaultLayout from "../layouts/DefaultLayout";
+import AdminLayout from "../layouts/AdminLayout";
+import NotFound from "../pages/NotFound";
+import { Navigate } from "react-router-dom";
+import { isAuthenticated } from "../utils/auth";
 
 // Lazy loading
-const About = lazy(() => import('../pages/About'));
-const AdminDashboard = lazy(() => import('../pages/AdminDashboard'));
-const Login = lazy(() => import('../pages/Login'));
-const Resume = lazy(() => import('../pages/Resume/index'));
-const Project = lazy(() => import('../pages/Projects'));
-const Contact = lazy(() => import('../pages/Contact'));
+const About = lazy(() => import("../pages/About"));
+const AdminDashboard = lazy(() => import("../pages/AdminDashboard"));
+const Login = lazy(() => import("../pages/Login"));
+const Resume = lazy(() => import("../pages/Resume/index"));
+const Project = lazy(() => import("../pages/Projects"));
+const Contact = lazy(() => import("../pages/Contact"));
 
 export interface AppRoute {
-    path: string;
-    element?: React.ReactNode;
-    layout?: React.ReactNode;
-    children?: AppRoute[];
-    requiresAuth?: boolean;
-    title?: string;
+  path: string;
+  element?: React.ReactNode;
+  layout?: React.ReactNode;
+  children?: AppRoute[];
+  requiresAuth?: boolean;
+  title?: string;
 }
 
 export const routesConfig: AppRoute[] = [
   {
-    path: '/',
+    path: "/",
     element: <Navigate to="/about" replace />,
   },
   {
-    path: '/',
+    path: "/",
     layout: <DefaultLayout />,
     children: [
-      { path: 'about', element: <About />, title: 'Viet Duc | Portfolio | About' },
+      {
+        path: "about",
+        element: <About />,
+        title: "Viet Duc | Portfolio | About",
+      },
     ],
   },
   {
-    path: '/admin',
+    path: "/admin",
     layout: <AdminLayout />,
     requiresAuth: true,
     children: [
-      { path: '', element: <AdminDashboard />, title: 'Admin Dashboard' },
+      { path: "", element: <AdminDashboard />, title: "Admin Dashboard" },
+      { path: "/resume", element: <Resume />, title: "My Resume" },
+      { path: "/project", element: <Project />, title: "My Projects" },
+      { path: "/contact", element: <Contact />, title: "Contact Me" },
     ],
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
-    title: 'Login',
+    title: "Login",
   },
   {
-    path: '/resume',
-    element: <Resume />,
-    title: 'Viet Duc | Portfolio | Resume'
-  },
-  {
-    path: '/project',
-    element: <Project />,
-    title: 'Viet Duc | Portfolio | Projects'
-  },
-  {
-    path: '/contact',
-    element: <Contact />,
-    title: 'Viet Duc | Portfolio | Contact'
-  },
-  {
-    path: '*',
+    path: "*",
     element: <NotFound />,
-    title: '404 - Page Not Found',
+    title: "404 - Page Not Found",
   },
-]; 
+];
